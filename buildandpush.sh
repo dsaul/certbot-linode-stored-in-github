0c245577d5b0e6bbda91516567b2c665289ff717
Dan Saul <dan@dsaul.ca>
2022-07-02 13:54:07 -0500

    buildandpush.sh

--- buildandpush.sh
+++ buildandpush.sh
@@ -7,7 +7,7 @@ set -o pipefail
 # Change Directory to script directory.
 cd $(dirname "${BASH_SOURCE[0]}")
 
-IMAGENAME="certbot-linode"
+IMAGENAME="certbot-linode-stored-in-github"
 TAGNAME="$(date +'%Y-%m-%d.%H-%M-%S.%N')" 
 TAGNAME2="$(date +'%Y-%m-%d')" 
 docker image build --no-cache \
